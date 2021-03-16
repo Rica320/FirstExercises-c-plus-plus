820ae7afc1bd0374fa4964c46dfc864b60ba7868
Ricardo Matos <ricardo.andre.de.matos@gmail.com>
2021-03-16 22:06:15 +0000

    Calendar

--- Module1/all module 2.cpp
+++ Module1/all module 2.cpp
@@ -77,7 +77,7 @@ double round(double& x, unsigned& n) {
 
 // exercise 3.5 - changed after 3.8
 
-
+/*
 int gcd(int a, int b) {
 	if (a < 0)
 		a = -a;
@@ -97,6 +97,7 @@ int gcd(int a, int b) {
 	}
 	return a;
 }
+*/
 
 
 // exercise 3.6.
@@ -135,6 +136,7 @@ bool readInt(int& x) {
 
 // exercise 3.8 using 3.5
 
+/*
 bool readFraction(int& numerator, int& denominator) {
 	if (denominator == 0) {
 		numerator = 0;
@@ -142,14 +144,17 @@ bool readFraction(int& numerator, int& denominator) {
 	}
 	return true;
 }
+
 void reduceFraction(int& numerator, int& denominator) {
 	int a = gcd(numerator, denominator);
 	numerator = numerator / a;
 	denominator = denominator / a;
 }
+
 bool validop(char ch) {
 	return (ch == '+' || ch == '-' || ch == '/' || ch == '*');
 }
+
 void performop(int& numerator, int& denominator, char ch, int d = 0, int n = 0, char lop = '+') {
 
 	// could improve code by adding a || and && operator
@@ -185,7 +190,107 @@ void performop(int& numerator, int& denominator, char ch, int d = 0, int n = 0, 
 		reduceFraction(numerator, denominator);
 	}
 }
+*/
 
+// exercise 3.9
+
+bool isLeapYear(int year) {
+	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+		return true;
+	return false;
+}
+
+int daysMonth(int month, int year) {
+	if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+		return 31;
+	else if (month == 2 && isLeapYear(year))
+		return 29;
+	else if (month == 2) {
+		return 28;
+	}
+	else
+		return 30;
+}
+
+int dayWeek(int year, int month, int day) {
+	int c;
+	switch (month)
+	{
+	case 1:
+		if (isLeapYear(year))
+			c = 6;
+		else
+			c = 0;
+		break;
+	case 2:
+		if (isLeapYear(year))
+			c = 2;
+		else
+			c = 3;
+		break;
+	case 3:
+		c = 3;
+		break;
+	case 4:
+		c = 6;
+		break;
+	case 5:
+		c = 1;
+		break;
+	case 6:
+		c = 4;
+		break;
+	case 7:
+		c = 6;
+		break;
+	case 8:
+		c = 2;
+		break;
+	case 9:
+		c = 5;
+		break;
+	case 10:
+		c = 0;
+		break;
+	case 11:
+		c = 3;
+		break;
+	case 12:
+		c = 5;
+		break;
+	default:
+		break;
+	}
+	// will not work to years < 1000 ?? it Works
+	return (5 * (year % 100) / 4 + c + day - 2 * ((year / 100) % 4) + 7) % 7;
+}
+
+string	mapMonth(int month) {
+	vector<string> vec = { "January", "February", "March", "April", "May", "June", "July", "August", "September", 
+		"October", "November", "December" };
+	return vec[month - 1];
+}
+
+void printMonth(int month, int year) {
+	// tansform month integer to names later - DONE
+	cout << mapMonth(month) << '/' << year << endl;
+	int first = dayWeek(year, month, 1);
+	int dayaMonth = daysMonth(month, year);
+	int k = (first - 1 + 7) % 7;
+	cout << "Sun" << setw(6) << "Mon" << setw(6) << "Tue" << setw(6) << "Wed" << setw(6) << "Thu" << setw(6) << "Fri" << setw(6) << "Sat" << endl;
+	cout << setw(k * 6 + 3);
+	for (int i = 1; i <= dayaMonth; i++, k = (k+1)%7) {
+		if (i != 1)
+			if (k == 0)
+				cout << setw(3) << i;
+			else
+				cout << setw(6) << i;
+		else
+			cout << i;
+		if (k == 6)
+			cout << endl;
+	}
+}
 
 int main() {
 
@@ -993,10 +1098,9 @@ int main() {
 			cout << x << endl;
 	} while (!cin.fail());
 	*/
-
 	// exercise 3.8
 
-
+	/*
 	int n, d;
 	char separator, lastop = '+';
 	int r1{}, r2{1};
@@ -1032,5 +1136,20 @@ int main() {
 		}
 	} while (!cin.fail());
 	cout << r1 << ' ' << r2;
+	*/
+
+	// exercise 3.9
+
+	int year;
+
+	cout << "year?";
+	cin >> year;
+	system("cls");
+
+	for (int i = 1; i <= 12; i++) {
+		printMonth(i, year);
+		cout << endl << endl;
+	}
+
 	return 0;
 }
