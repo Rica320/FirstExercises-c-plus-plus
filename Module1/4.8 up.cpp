2e1c2edabc9b7a855513dc15cc958a6d5d1942c4
Ricardo Matos <ricardo.andre.de.matos@gmail.com>
2021-03-30 17:21:25 +0100

    4.12

--- Module1/4.8 up.cpp
+++ Module1/4.8 up.cpp
@@ -10,6 +10,33 @@ using namespace std;
 int as(const void* x, const void* y) {
 	return  (*(int*)x - *(int*)y);
 }
+void randFillArrPointer(int* a, size_t x) {
+	srand(time(NULL));
+	for (size_t i = 0; i < x; i++) {
+		for (size_t j = 0; j < x; j++) {
+			a[i*x + j + 1] = rand() % 10;
+		}
+	}
+}
+void showArrayPointer(int* a, size_t x) {
+	cout << endl;
+	for (size_t i = 0; i < x; i++) {
+		for (size_t j = 0; j < x; j++) {
+			cout << setw(2) << a[i * x + j + 1];
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
+// exercise 4.12
+
+void readArray(int* a, size_t nElem);
+int findValueInArray(const int* a, size_t nElem, int value, size_t pos1, size_t pos2 = -1);
+size_t findMultValuesInArray(const int* a, size_t nElem, int value, size_t pos1, size_t pos2, size_t* index);
+
+int findValueInArray(const int* pos1, const int* pos2, int value);
+size_t findMultValuesInArray(const int* pos1, const int* pos2, int value, size_t* index);
 
 int main() {
 
@@ -49,6 +76,7 @@ int main() {
 
 	//exercise 4.11
 
+	/*
 	size_t x;
 
 	cout << "len of square Matrix ? ";
@@ -64,12 +92,12 @@ int main() {
 	randomArrayFill(a, x);
 	showArray(a, x);
 	
-	// TODO : Sort based on colums too 
+	// TODO : Sort based on colums too - DONE
 
-	/*for (size_t i = 0; i < x; i++) {
-		qsort(a[i], x, sizeof(int), as);
-	}
-	showArray(a, x);*/
+	//for (size_t i = 0; i < x; i++) {
+	//	qsort(a[i], x, sizeof(int), as);
+	//}
+	//showArray(a, x);
 
 	// c)
 
@@ -79,11 +107,98 @@ int main() {
 	for (size_t i = 0; i < x; i++) {
 		qsort(a[i] + x/2 , x - (x / 2), sizeof(int), as);
 	}
-
 	showArray(a, x);
 
+	//int* v = (int*)malloc(sizeof(int) * x);
+	//randFillArrPointer(v, x);
+	//showArrayPointer(v, x);
+	//qsort(v, x*x, sizeof(int), as);
+	//showArrayPointer(v, x);
+	*/
+
+	// exercise 4.12
+
+	size_t x;
+	cout << "Lenght of array ... " << endl; cin >> x;
+
+	int* a = (int*) malloc(sizeof(int) * x);
+	readArray(a, x);
+	cout << findValueInArray(a, x, 1, 0, 3) << endl;
+	cout << findValueInArray(a, a + x, 1) << endl;
+
+	size_t* r = (size_t*) malloc(sizeof(size_t) * x);
+	size_t elements = findMultValuesInArray(a, x, 1, 0, 5, r);
+	for (size_t i = 0; i < elements; i++)
+		cout << r[i] << " , ";
+
+	cout << endl;
+
+	size_t* d = (size_t*)malloc(sizeof(size_t) * x);
+	size_t element = findMultValuesInArray(a,a+x,1,d);
+	for (size_t i = 0; i < element; i++)
+		cout << d[i] << " , ";
 
 	return 0;
 }
 
-// exercise 4.11
+
+// exercise 4.12
+
+void readArray(int* a, size_t nElem) {
+	for (size_t i = 0; i < nElem; i++) {
+		cout << i << " ?"; cin >> a[i];
+	}
+}
+int findValueInArray(const int* a, size_t nElem, int value, size_t pos1, size_t pos2) {
+	if (pos2 == -1)
+		pos2 = nElem - 1;
+	// changed ...
+	for (size_t i = pos1; i <= pos2; i++) {
+		if (a[i] == value) {
+			return i;
+		}
+	}
+}
+size_t findMultValuesInArray(const int* a, size_t nElem, int value, size_t pos1, size_t pos2, size_t* index) {
+	size_t sum{};
+	// putting another variable to help with the index of the array index[] would be better
+	for (size_t x = 0; x < nElem; x++) {
+		for (size_t i = pos1; i <= pos2; i++) {
+			if (a[i] == value) {
+				index[x] = i;
+				sum++;
+				pos1 = i + 1;
+				break;
+			}
+		}
+	}
+	return sum;
+}
+
+int findValueInArray(const int* pos1, const int* pos2, int value) {
+	// return the index based on the interval given
+	int nElem = (pos2 - pos1);
+	// changed ...
+	for (int i = 0; i <= nElem; i = i ++) {
+		if (*(pos1 + i) == value) {
+			return i;
+		}
+	}
+}
+size_t findMultValuesInArray(const int* pos1, const int* pos2, int value, size_t* index) {
+	size_t sum{};
+	size_t nElem = (pos2 - pos1);
+	size_t x1{};
+	// putting another variable to help with the index of the array index[] would be better
+	for (size_t x = 0; x < nElem; x++) {
+		for (size_t i = x1; i <= nElem; i++) {
+			if (*(pos1 + i) == value) {
+				index[x] = i;
+				sum++;
+				x1 = i + 1;
+				break;
+			}
+		}
+	}
+	return sum;
+}
